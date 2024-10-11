d97d09b6567024aecea36b2262bf5eeb2b32eb61
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-10-11 11:24:36 -0300

    react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  
+
+
+  return (
+  <div className="App">
+    <h1>Teste</h1>
+    <p>Esse é a minha primeira alteração no app</p>
+  </div>
+  );
+}
+
+export default App;
