e22e79de98b15f8b52d1be6b59f9e9c4936c3859
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-10-15 11:45:42 -0300

    su

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 function App() {
 
-  
+  function soma(a, b){
+    return a + b 
+  }
 
 
   return (
   <div className="App">
     <h1>Teste</h1>
     <p>Esse é a minha primeira alteração no app</p>
+    <p>Soma: {soma(1,2)}</p> 
   </div>
   );
 }
